4620dd3977c34a6a1e6c0cfe06149463d71be591
rawaanalshayji <66734178+rawaanalshayji@users.noreply.github.com>
2020-06-28 03:23:14 +0300

    Added XO

---
+++ ios-cw5-p2/iOS-cw-5-p2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-cw-5-p2
+//
+//  Created by Rawaan Alshayji on 6/28/20.
+//  Copyright © 2020 Rawaan Alshayji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
