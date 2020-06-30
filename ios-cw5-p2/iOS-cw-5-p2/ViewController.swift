f7d450756fffee8ea1ac9353cd97086ea3923f03
rawaanalshayji <66734178+rawaanalshayji@users.noreply.github.com>
2020-06-30 09:04:16 +0300

    done
    redid it

--- ios-cw5-p2/iOS-cw-5-p2/ViewController.swift
+++ ios-cw5-p2/iOS-cw-5-p2/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-   
+    
+    
     @IBOutlet weak var b1: UIButton!
     @IBOutlet weak var b2: UIButton!
     @IBOutlet weak var b3: UIButton!
@@ -20,13 +20,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var b7: UIButton!
     @IBOutlet weak var b8: UIButton!
     @IBOutlet weak var b9: UIButton!
-  
-    @IBOutlet weak var turnLable: UILabel!
     
-     var counter = 0
+    @IBOutlet weak var turnLable: UILabel!
+    var buttons: [UIButton] = []
+    var counter = 0
+    
+    override func viewDidLoad() {
+        buttons = [b1, b2, b3, b4, b5, b6, b7, b8, b9]}
     @IBAction func press(_ sender: UIButton) {
         
-       
+        
         if counter % 2 == 0{
             sender.setTitle("x", for:.normal)
             sender.setTitleColor(.white, for: .normal)
@@ -35,104 +38,55 @@ class ViewController: UIViewController {
         else{
             sender.setTitle("O", for:.normal)
             sender.setTitleColor(.black, for: .normal)
-             turnLable.text = "X Turn"
+            turnLable.text = "X Turn"
         }
         
         counter += 1
         sender.isEnabled = false
-      
         
+        checkWinner(p: "X")
+        checkWinner(p: "O")
         
+    }
+    
+    func checkWinner(p: String){
+        let r1 = b1.titleLabel?.text == p && b2.titleLabel?.text == p && b3.titleLabel?.text == p
+        let r2 = b4.titleLabel?.text == p && b5.titleLabel?.text == p && b6.titleLabel?.text == p
+        let r3 = b7.titleLabel?.text == p && b8.titleLabel?.text == p && b9.titleLabel?.text == p
+        let c1 = b1.titleLabel?.text == p && b4.titleLabel?.text == p && b7.titleLabel?.text == p
+        let c2 = b2.titleLabel?.text == p && b5.titleLabel?.text == p && b8.titleLabel?.text == p
+        let c3 = b3.titleLabel?.text == p && b6.titleLabel?.text == p && b9.titleLabel?.text == p
+        let d1 = b1.titleLabel?.text == p && b5.titleLabel?.text == p && b9.titleLabel?.text == p
+        let d2 = b3.titleLabel?.text == p && b5.titleLabel?.text == p && b7.titleLabel?.text == p
         
-        
-        if (b1.titleLabel?.text == "X" && b2.titleLabel?.text == "X" && b3.titleLabel?.text == "X") || (b4.titleLabel?.text == "X" && b5.titleLabel?.text == "X" && b6.titleLabel?.text == "X") || (b7.titleLabel?.text == "X" && b8.titleLabel?.text == "X" && b9.titleLabel?.text == "X") || (b1.titleLabel?.text == "X" && b4.titleLabel?.text == "X" && b7.titleLabel?.text == "X") || (b2.titleLabel?.text == "X" && b5.titleLabel?.text == "X" && b8.titleLabel?.text == "X") || (b3.titleLabel?.text == "X" && b6.titleLabel?.text == "X" && b9.titleLabel?.text == "X") || (b1.titleLabel?.text == "X" && b5.titleLabel?.text == "X" && b9.titleLabel?.text == "X") || (b3.titleLabel?.text == "X" && b5.titleLabel?.text == "X" && b7.titleLabel?.text == "X")
-        {
-            print("X is the winner")
+        if r1 || r2 || r3 || c1 || c2 || c3 || d1 || d2 {
             
+            print("\(p) is the winner")
             
-            let alertController = UIAlertController(title: "X WINS", message: "press the button to restart the game", preferredStyle: .alert)
-            present(alertController, animated: true, completion: nil)
+            let alertController = UIAlertController(title: "\(p) WINS", message: "press the button to restart the game", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "Ok", style: .cancel)
+            {(alert) in self.restartGame()}
             alertController.addAction(okAction)
             present(alertController, animated: true, completion: nil)
-            
-            
         }
-
-      
-        if (b1.titleLabel?.text == "O" && b2.titleLabel?.text == "O" && b3.titleLabel?.text == "O") || (b4.titleLabel?.text == "O" && b5.titleLabel?.text == "O" && b6.titleLabel?.text == "O") || (b7.titleLabel?.text == "O" && b8.titleLabel?.text == "O" && b9.titleLabel?.text == "O") || (b1.titleLabel?.text == "O" && b4.titleLabel?.text == "O" && b7.titleLabel?.text == "O") || (b2.titleLabel?.text == "O" && b5.titleLabel?.text == "O" && b8.titleLabel?.text == "O") || (b3.titleLabel?.text == "O" && b6.titleLabel?.text == "O" && b9.titleLabel?.text == "O") || (b1.titleLabel?.text == "O" && b5.titleLabel?.text == "O" && b9.titleLabel?.text == "O") || (b3.titleLabel?.text == "O" && b5.titleLabel?.text == "O" && b7.titleLabel?.text == "O")
-               {
-                   print("O is the winner")
-                
-                let alertController = UIAlertController(title: "O WINS", message: "press the button to restart the game", preferredStyle: .alert)
-                present(alertController, animated: true, completion: nil)
-                let okAction = UIAlertAction(title: "Ok", style: .cancel)
-                alertController.addAction(okAction)
-                present(alertController, animated: true, completion: nil)
-                
-               }
-
-        
-        
-        
-        
-        
-   
-        
-    
-       
-    
     }
+    
+    
     @IBAction func resetTapped() {
-        
-       func restartGame()
-       {
-           b1.setTitle("", for: .normal)
-           b1.titleLabel?.text = ""
-           b1.isEnabled = true
-           
-           b2.setTitle("", for: .normal)
-           b2.titleLabel?.text = ""
-           b2.isEnabled = true
-           
-           b3.setTitle("", for: .normal)
-           b3.titleLabel?.text = ""
-           b3.isEnabled = true
-           
-           b4.setTitle("", for: .normal)
-           b4.titleLabel?.text = ""
-           b4.isEnabled = true
-           
-           b5.setTitle("", for: .normal)
-           b5.titleLabel?.text = ""
-           b5.isEnabled = true
-           
-           b6.setTitle("", for: .normal)
-           b6.titleLabel?.text = ""
-           b6.isEnabled = true
-           
-           b7.setTitle("", for: .normal)
-           b7.titleLabel?.text = ""
-           b7.isEnabled = true
-           
-           b8.setTitle("", for: .normal)
-           b8.titleLabel?.text = ""
-           b8.isEnabled = true
-           
-           b9.setTitle("", for: .normal)
-           b9.titleLabel?.text = ""
-           b9.isEnabled = true
-       
-           turnLable.text = "X TURN"
-           
-           
-           
-           
-           
-           
-       }
-        
-        
+        restartGame()
     }
+    
+    func restartGame()
+    {
+        for b in buttons {
+            b.setTitle("", for: .normal)
+            b.titleLabel?.text = ""
+            b.isEnabled = true
+        }
+        turnLable.text = "X turn"
+    }
+    
 }
+
+
 
